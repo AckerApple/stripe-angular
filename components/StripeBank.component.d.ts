97d0087e9ad47428599816ef0d765f9c0d64049f
Admin Apple <adminapple@ackerlaptop.local>
2018-10-29 16:04:23 -0400

    fix

--- components/StripeBank.component.d.ts
+++ components/StripeBank.component.d.ts
@@ -2,6 +2,14 @@ import { EventEmitter, ElementRef } from "@angular/core";
 import { StripeInstance, StripeCardOptions } from "../StripeTypes";
 import { StripeScriptTag } from "../StripeScriptTag";
 import { StripeToken } from "../StripeTypes";
+export interface bank_account {
+    country: string;
+    currency: string;
+    routing_number: string;
+    account_number: string;
+    account_holder_name: string;
+    account_holder_type: string;
+}
 export declare class StripeBank {
     ElementRef: ElementRef;
     StripeScriptTag: StripeScriptTag;
