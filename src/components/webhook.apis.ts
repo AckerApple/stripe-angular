43e4985892719ef9814ac21ad401b097d5d41910
Acker Apple <y>
2022-02-09 10:25:09 -0500

    update

--- src/components/webhook.apis.ts
+++ src/components/webhook.apis.ts
@@ -24,6 +24,73 @@ export const webhookPost: ISimpleRouteEditor = {
   },
   data: {},
   pastes: [{
+    title: 'payment_intent.canceled',
+    pasteKey: 'data',
+    value: {
+      "created": 1326853478,
+      "livemode": false,
+      "id": "evt_00000000000000",
+      "type": "payment_intent.canceled",
+      "object": "event",
+      "request": null,
+      "pending_webhooks": 1,
+      "api_version": "2020-08-27",
+      "data": {
+        "object": {
+          "id": "pi_00000000000000",
+          "object": "payment_intent",
+          "amount": 1000,
+          "amount_capturable": 0,
+          "amount_received": 0,
+          "application": null,
+          "application_fee_amount": null,
+          "canceled_at": null,
+          "cancellation_reason": null,
+          "capture_method": "automatic",
+          "charges": {
+            "object": "list",
+            "data": [
+            ],
+            "has_more": false,
+            "url": "/v1/charges?payment_intent=pi_0Hoycz7ENYdnrElvAwYEh0Qu"
+          },
+          "client_secret": "pi_0Hoycz7ENYdnrElvAwYEh0Qu_secret_1jqN50lmxFI5zD0D3kTsVqqsd",
+          "confirmation_method": "automatic",
+          "created": 1605736549,
+          "currency": "usd",
+          "customer": null,
+          "description": "Created by stripe.com/docs demo",
+          "invoice": null,
+          "last_payment_error": null,
+          "livemode": false,
+          "metadata": {
+          },
+          "next_action": null,
+          "on_behalf_of": null,
+          "payment_method": null,
+          "payment_method_options": {
+            "card": {
+              "installments": null,
+              "network": null,
+              "request_three_d_secure": "automatic"
+            }
+          },
+          "payment_method_types": [
+            "card"
+          ],
+          "receipt_email": null,
+          "review": null,
+          "setup_future_usage": null,
+          "shipping": null,
+          "statement_descriptor": null,
+          "statement_descriptor_suffix": null,
+          "status": "canceled",
+          "transfer_data": null,
+          "transfer_group": null
+        }
+      }
+    }
+  }, {
     title: 'source.chargeable',
     pasteKey: 'data',
     value: {
